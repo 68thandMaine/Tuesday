16e4e4a397c2a74f2c76859ff5735ccc5c2a34e2
Student <example@example.com>
2018-10-16 10:54:43 -0700

    Add change background color code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
+// $(document).ready(function() {
+//   $(".clickable").click(function() {
+//     $("#walrus-showing").toggle();
+//     $("#walrus-hidden").toggle();
+//   });
+//
+//   $(".clickable-fade").click(function() {
+//     $("#walrus-fadein").fadeToggle();
+//
+//   });
+//
+//
+// });
+
+// Under is another lesson //
+
+
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
